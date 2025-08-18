fbf1de8d7c2b0c1ad3eab09072dab6a580a02608
Mary <njuemarywanjiru@gmail.com>
2025-08-18 13:16:12 +0300

    fixed allow restrictions

--- backend/src/main/kotlin/com/littlelearners/backend/config/WebConfig.kt
+++ backend/src/main/kotlin/com/littlelearners/backend/config/WebConfig.kt
@@ -9,7 +9,10 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // Apply CORS to all endpoints in your API
-            .allowedOrigins("*",) // Allow requests from your React app's origin
+            .allowedOriginPatterns(
+                "https://*.ngrok-free.app",   // allow ngrok tunnel
+                "little-learners-three.vercel.app" // your Vercel frontend
+            ) // Allow requests from your React app's origin
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow common HTTP methods
             .allowedHeaders("*") // Allow all headers
             .allowCredentials(true) // Allow credentials (like cookies, authorization headers)
