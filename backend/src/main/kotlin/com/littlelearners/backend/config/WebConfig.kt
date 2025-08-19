d789aaa5690dffffb1b4e1de9a86fb4c6a1ed565
Mary <njuemarywanjiru@gmail.com>
2025-08-19 14:51:30 +0300

    added a docker file

--- backend/src/main/kotlin/com/littlelearners/backend/config/WebConfig.kt
+++ backend/src/main/kotlin/com/littlelearners/backend/config/WebConfig.kt
@@ -1,5 +1,4 @@
 package com.littlelearners.backend.config
-
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -8,14 +7,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**") // Apply CORS to all endpoints in your API
+        registry.addMapping("/**")
             .allowedOriginPatterns(
-                "https://*.ngrok-free.app",   // allow ngrok tunnel
-                "little-learners-three.vercel.app" // your Vercel frontend
-            ) // Allow requests from your React app's origin
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow common HTTP methods
-            .allowedHeaders("*") // Allow all headers
-            .allowCredentials(true) // Allow credentials (like cookies, authorization headers)
-            .maxAge(3600); // Max age of the CORS pre-flight request
+                "http://localhost:3000",
+                "https://*.ngrok-free.app",             // any ngrok subdomain
+                "https://little-learners-three.vercel.app" // your Vercel frontend
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
     }
 }
